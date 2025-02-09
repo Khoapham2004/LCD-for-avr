0ef644ea19eac3215c14c32acb686b6fa820b0c3
Khoapham2004 <phamdangkhoa544@gmail.com>
2025-02-09 14:52:33 +0700

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,18 @@
 int i,j;
 char intro[] = "                    HELLO WORLD                ";
 int length = sizeof(intro) - 1;
-
-void LCD_displacement() {
+void LCD_displacementLCD() {
+    for (i = 0; i <= length - 16; i++) {
+        lcd_clear();
+        for (j = 0; j < 16; j++) {       
+            lcd_gotoxy(j, 0);               
+            lcd_putchar(intro[i + j]);  
+            _delay_us(100);     
+        }
+        _delay_ms(200);                      
+    }
+}
+void LCD_displacementI2C() {
     for (i = 0; i <= length - 16; i++) {
         lcd_clear();
         for (j = 0; j < 16; j++) {       
@@ -18,7 +28,19 @@ void LCD_displacement() {
 }
 
 int main(void)
-{
+{   
+    lcd_set_pins(
+        (LCD_Pin){&PORTB, &DDRB, PB0},  
+        (LCD_Pin){&PORTB, &DDRB, PB1},  
+        (LCD_Pin){&PORTB, &DDRB, PB2},  
+        (LCD_Pin){&PORTB, &DDRB, PB3},  
+        (LCD_Pin){&PORTL, &DDRL, PL0},  
+        (LCD_Pin){&PORTL, &DDRL, PL1},  
+        (LCD_Pin){&PORTL, &DDRL, PL2}   
+    );
+
+    lcd_init();
+	
     i2c_init();
 	i2c_start();
 	i2c_write(0x70);
@@ -26,7 +48,9 @@ int main(void)
 	
     while (1) 
     {
-	   LCD_displacement();
+	  LCD_displacementLCD();
+	  LCD_displacementI2C();
+	    
     }
 }
 
